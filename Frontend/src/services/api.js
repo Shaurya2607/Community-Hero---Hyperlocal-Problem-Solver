7fca858b9f196ba71cf7bf82e01d67525b4afc79
SHAURYA GANGWAR <shauryagangwar.2607@gmail.com>
2026-06-29 08:44:33 +0530

    Connect frontend to deployed backend

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://community-hero-hyperlocal-problem-solver-irta.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
